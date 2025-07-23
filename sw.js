9fd572d2e9c92477dbc892aa3dda18b56685df67
Augusto <AugustoMPantaloni@gmail.com>
2025-07-23 16:24:45 -0300

    StockLeroma

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'stock-leroma-cache-v1';
+const urlsToCache = [
+  '.',
+  './index.html',
+  './logoColor.png',
+  // si usás otros archivos (css, js) los agregás acá
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // Limpiar caches viejos si querés, opcional
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+});
